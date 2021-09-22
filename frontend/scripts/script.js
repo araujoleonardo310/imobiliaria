6354566aba46b3c86e7107d3c8aaffe693305972
araujoleonardo310 <araujoleonardo310@gmail.com>
2021-09-22 14:32:44 -0300

    Arquivos axiios e json adicionados

---
+++ frontend/scripts/script.js
@@ -0,0 +1,28 @@
+var axios = "http://localhost:3000";
+
+
+function pegarDados (){
+    const inputName = document.querySelector("#nome");
+    const nome = inputName.value
+
+    const inputData = document.querySelector("#data");
+    const data = inputData.value
+
+    const inputValor = document.querySelector("#valor");
+    const valor = inputValor.value
+
+    const selectStatus = document.querySelector("#status");
+    const status = selectStatus.value
+
+    module.exports = () => {
+        const data = { aluguel: [] }
+        // Create 1000 users
+        for (let i = 0; i < 1000; i++) {
+          data.aluguel.push({ id: i, nome: `aluguel${nome}` })
+        }
+        return data
+      }
+}
+
+
+
