74d38749de8dee3ec4425fa01b466392340c77f2
Leonardo Araújo <araujoleonardo310@gmail.com>
2021-09-30 08:19:45 -0300

    Corrigido db.json

--- frontend/scripts/editar.js
+++ frontend/scripts/editar.js
@@ -5,13 +5,26 @@ const Editar = async(id) => {
   window.location.href = '/frontend/pages/form.html';
 
   dadosEdicao.data.map((item) => { 
-    document.getElementById("nome").value = `${item.nome}`;
+    var form = document.getElementById('form');
+    var inputName = document.createElement("input");
+    var inputData = document.createElement("input");
+    var inputValor = document.createElement("input")
+    var inputStatus = document.createElement('select')
+
+    var nome = document.createTextNode(`${item.nome}`);
+    var data = document.createTextNode(`${item.data}`);
+    var valor = document.createTextNode(`${item.valor}`);
+    var status = document.createTextNode(`${item.status}`);
+  
+    form.appendChild(inputName);
+    inputName.appendChild(nome);
+   
 
     
   });
 };
 
-list();
+
   
   
 
