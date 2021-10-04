acc553e05c14af538f563f6c39b88d30f7d93a55
Leonardo Araújo <araujoleonardo310@gmail.com>
2021-10-04 06:22:47 -0300

    Design concluído

--- frontend/scripts/enviar.js
+++ frontend/scripts/enviar.js
@@ -1,6 +1,67 @@
 var api = "http://localhost:3000";
 
-const form = document.getElementById('form')
+const Create = async () => {
+      const inputName = document.getElementById("nome");
+      const valueNome = inputName.value;
+  
+      const inputData = document.getElementById("data");
+      const valueData = inputData.value;
+  
+      const inputValor = document.getElementById("valor");
+      const valueValor = inputValor.value;
+  
+      const selectStatus = document.getElementById("status");
+      const valueStatus = selectStatus.value;
+  
+      var dados = {
+          nome: valueNome, 
+          data: valueData, 
+          valor: valueValor,
+          status: valueStatus 
+      };
+      
+      await axios.post(`${api}/aluguel`, dados);  
+
+      $("input").click(function(e) {
+					
+        e.preventDefault();
+        
+    });
+};
+
+
+  	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const form = document.getElementById('form')
 form.addEventListener('submit', e => {
     e.preventDefault()
 
@@ -23,6 +84,12 @@ form.addEventListener('submit', e => {
         status: valueStatus
     };
 
-    await axios.post(`${api}/aluguel`, dados);
+    return await axios.post(`${api}/aluguel`, dados);
 })
+
+function Enviar() {
+    return form
+}
+
+*/
 
