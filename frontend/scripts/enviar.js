482487f6fc878fbe25d912d043e707f9f41c2071
araujoleonardo310 <araujoleonardo310@gmail.com>
2021-10-04 09:41:04 -0300

    corrigido preventDefault e mudanças no form

--- frontend/scripts/enviar.js
+++ frontend/scripts/enviar.js
@@ -21,75 +21,20 @@ const Create = async () => {
       };
       
       await axios.post(`${api}/aluguel`, dados);  
-
-      $("input").click(function(e) {
-					
-        e.preventDefault();
-        
-    });
 };
 
 
-  	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const form = document.getElementById('form')
-form.addEventListener('submit', e => {
-    e.preventDefault()
-
-    const inputName = document.getElementById("nome");
-    const valueNome = inputName.value;
-
-    const inputData = document.getElementById("data");
-    const valueData = inputData.value;
-
-    const inputValor = document.getElementById("valor");
-    const valueValor = inputValor.value;
-
-    const selectStatus = document.getElementById("status");
-    const valueStatus = selectStatus.value;
-
-    var dados = {
-        nome: valueNome,
-        data: valueData,
-        valor: valueValor,
-        status: valueStatus
-    };
-
-    return await axios.post(`${api}/aluguel`, dados);
-})
-
 function Enviar() {
-    return form
+    const wes = document.querySelector("form");
+        wes.addEventListener("click", function(event) {
+        console.log(event);
+    });
+
+    return Create()
 }
 
-*/
+
+
+
+
 
